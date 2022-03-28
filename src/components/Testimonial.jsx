aca9468ef454b7179e2b492a29f423343d9c6d92
killyvera <david.esmeralda@gmail.com>
2022-03-28 12:46:55 -0600

    first commit

---
+++ src/components/Testimonial.jsx
@@ -0,0 +1,31 @@
+/** @jsxImportSource @emotion/react */
+import tw from 'twin.macro';
+import gitIcon from '../svg/icon-github.svg';
+import linkedinIcon from '../svg/icon-linkedin.svg';
+import twitterIcon from '../svg/icon-twitter.svg';
+
+function Testimonial(){
+    return (
+        <div tw=' shadow-md m-auto mt-5 w-[18rem] p-5 bg-[#ffd543] rounded-sm '>
+            <div tw='' >
+                <a tw='block relative ' >
+                <img tw='mx-auto object-cover rounded-full h-32 w-32 shadow-lg ' src= {require('../images/01.JPG')} />
+                </a>
+                <div tw='flex gap-1 justify-center pt-3' >
+                <img src= {gitIcon} />
+                <img src= {linkedinIcon} />
+                <img src= {twitterIcon} />
+                </div>
+            </div>
+            <div tw='text-center' >
+                <p tw='text-2xl text-gray-700 pt-1' > Jhon Doe </p>
+                <p tw='text-xl text-gray-500 font-normal ' > Full Stack Developer </p>
+                <p tw='text-lg text-gray-500 font-light pt-2 '> "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..." </p>
+            </div>
+
+        </div>
+    )
+
+}
+
+export default Testimonial;
